6d9b4d264cedcafd2c1ed56ed4cadf0c6a5423c5
bohdan <bogdanmorozuk72@gmail.com>
2023-09-05 20:15:24 +0300

    Model add

--- jewelry_app/models.py
+++ jewelry_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+# Model for product category
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Model for product
+class Jewelry(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='jewelry_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
